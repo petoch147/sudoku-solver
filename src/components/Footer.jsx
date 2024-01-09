327d213c3688a83906029c5c76ad31c0bba2b9b9
petoch147 <petoch147@gmail.com>
2024-01-09 22:25:44 +0500

    commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer class="w-full absolute bottom-1 text-center">
+      Made with ♥
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 class="text-3xl m-5 text-center">
+        Sudoku Solver
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
